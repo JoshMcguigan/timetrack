902bc810ade4484bf05568195a5717528bb7c87d
Josh Mcguigan <joshmcg88@gmail.com>
2018-08-20 18:40:56 -0700

    Updated config to store raw data in OS correct location

--- src/config/mod.rs
+++ src/config/mod.rs
@@ -1,5 +1,7 @@
 use directories::ProjectDirs;
 use std::path::PathBuf;
+use std::fs::OpenOptions;
+use std::fs;
 
 pub struct Configuration {
     pub track_paths: Vec<PathBuf>,
@@ -7,18 +9,28 @@ pub struct Configuration {
 }
 
 pub fn get_config() -> Configuration {
-//    let project_dir = ProjectDirs::from(
-//        "rust",
-//        "cargo",
-//        "timetrack"
-//    ).expect("Failed to read project directories");
-//
-//    let raw_data_path = project_dir.data_local_dir();
+    let project_dir = ProjectDirs::from(
+        "rust",
+        "cargo",
+        "timetrack"
+    ).expect("Failed to read project directories");
+
+    let raw_data_directory = project_dir.data_local_dir();
+    let raw_data_file_path = raw_data_directory.join(".timetrack_raw");
+
+    fs::create_dir_all(&raw_data_directory)
+        .expect("Failed to create raw data directory");
+    OpenOptions::new()
+        .create(true)
+        .read(true)
+        .write(true)
+        .open(&raw_data_file_path)
+        .expect("Failed to create raw data file");
 
     Configuration {
         // TODO how to handle two track paths where one is a subdirectory of another
         track_paths: vec![PathBuf::from("/Users/josh/Projects")],
-        raw_data_path: PathBuf::from("/Users/josh/.timetrack_raw"),
+        raw_data_path: raw_data_file_path,
     }
 }
 
