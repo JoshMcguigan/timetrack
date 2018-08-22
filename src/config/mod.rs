9c658d57bcc0ea731a18a9aa2bbc038564880952
Josh Mcguigan <joshmcg88@gmail.com>
2018-08-21 19:12:11 -0700

    Corrected config defaults and implemented display trait

--- src/config/mod.rs
+++ src/config/mod.rs
@@ -1,4 +1,5 @@
 use directories::ProjectDirs;
+use directories::BaseDirs;
 use std::path::PathBuf;
 use std::fs::OpenOptions;
 use std::fs;
@@ -7,10 +8,22 @@ use std::path::Path;
 use std::io::Write;
 use std::io::Read;
 use toml;
+use std::fmt::Display;
+use std::fmt;
+use std::fmt::Formatter;
 
 pub struct Configuration {
     pub track_paths: Vec<PathBuf>,
     pub raw_data_path: PathBuf
+}
+
+impl Display for Configuration {
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+        write!(f, "TimeTrack Configuration\nTracking paths: {:?}\nRaw data path: {:?}",
+            self.track_paths,
+            self.raw_data_path,
+        )
+    }
 }
 
 #[derive(Deserialize, Serialize)]
@@ -37,8 +50,8 @@ pub fn get_config() -> Configuration {
 
 impl<'a> TimeTracker<'a> {
     pub fn print_config(&self) {
-        // TODO print relevant configuration details
-        println!("Configuration");
+        // TODO also print config file path
+        println!("{}", self.config);
     }
 }
 
@@ -78,7 +91,11 @@ fn get_user_config(project_dirs: &ProjectDirs) -> UserConfig {
     toml::from_str(&contents).expect("Failed to parse config file as TOML")
 }
 
-fn init_config_file(config_dir: impl AsRef<Path>, config_file_path: impl AsRef<Path>) {
+fn init_config_file(
+        config_dir: impl AsRef<Path>,
+        config_file_path: impl AsRef<Path>
+    ) {
+
     fs::create_dir_all(&config_dir)
         .expect("Failed to create config directory");
     let mut f = OpenOptions::new()
@@ -88,9 +105,12 @@ fn init_config_file(config_dir: impl AsRef<Path>, config_file_path: impl AsRef<P
         .open(&config_file_path)
         .expect("Failed to create config file");
 
-    // TODO set this default to users HOME directory
+    let home_dir = BaseDirs::new()
+                            .expect("Unable to find home directory")
+                            .home_dir()
+                            .to_owned();
     let default_config = UserConfig {
-        track_paths: vec![PathBuf::from("/Users/josh/Projects")]
+        track_paths: vec![home_dir]
     };
 
     write!(&mut f, "{}", toml::to_string(&default_config).expect("Failed to convert default user config to TOML"));
