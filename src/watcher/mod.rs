7b0611f8941e11bcc69165f77330a3a646663ee3
Josh Mcguigan <joshmcg88@gmail.com>
2018-09-11 04:32:31 -0700

    Corrected import for cross platform support

--- src/watcher/mod.rs
+++ src/watcher/mod.rs
@@ -2,10 +2,9 @@ use notify::{DebouncedEvent, RecommendedWatcher, RecursiveMode, Watcher};
 use std::path::PathBuf;
 use std::time::Duration;
 use std::sync::mpsc::Sender;
-use notify::FsEventWatcher;
 use notify;
 
-pub fn get_watcher(track_path: &PathBuf, tx: Sender<DebouncedEvent>) -> Result<FsEventWatcher, notify::Error> {
+pub fn get_watcher(track_path: &PathBuf, tx: Sender<DebouncedEvent>) -> Result<RecommendedWatcher, notify::Error> {
     let mut watcher: RecommendedWatcher =
         Watcher::new(tx, Duration::from_secs(0))?;
     watcher.watch(track_path, RecursiveMode::Recursive)?;
